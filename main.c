5915ea4d637475c3a4d18cdb14eb152957eaa7ed
Abdelrahman-Momen <132786295+Abdelrahman-Momen@users.noreply.github.com>
2024-05-02 02:38:39 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,7 @@
-#include "GPIO.h"
-#include "UART.h"
 #include "PORTF.h"
-#include "string.h"
-#include "I2C.h"
-#include "LCD.h"
-#include "SYSTICK.h"
 #include "gps.h"
+
+uint8_t Switch1State, oldSwitch1State = 1;
 
 int main()
 {
@@ -13,12 +9,9 @@ int main()
 	HAL_PORTF_LEDS_Init();
 	while(1)
 	{
-		uint8_t Switch1 = (uint8_t)HAL_PORTF_GetSwitchValue(SW1);
-		if(!Switch1){
-			HAL_PORTF_SetLedValue(GREEN,LED_ON);
-		}
-		else{
-			HAL_PORTF_SetLedValue(GREEN,LED_OFF);
+		Switch1State = HAL_PORTF_GetSwitchValue(SW1);
+		if(!Switch1State , oldSwitch1State){
+			HAL_PORTF_led_Toggle(GREEN);
 		}
 	}
 }
