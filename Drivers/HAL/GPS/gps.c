0894730cacf5983503ee7b916991412e7a51a8f7
Mohamed Elsheshtawy <120197885+MElsheshtawy@users.noreply.github.com>
2024-05-12 22:56:42 +0300

    Add files via upload
    GPS updated after some modifications to be used and read easily.

--- Drivers/HAL/GPS/gps.c
+++ Drivers/HAL/GPS/gps.c
@@ -8,55 +8,45 @@
 #include <math.h>
 #include <string.h>
 #include "UART.h"
-float currentLat;
-float currentlong;
+static float currentLat;
+static float currentlong;
 
-char*token;
-
-
-char GPS[80];
-char GPS_formated[12][20];
-
-void MCAL_UART_INIT(UART0);// init uart0 for port A  
+char* token;
 
 
+char GPS[100];
+char GPS_formated[15][30];
 char GPS_LogName[] = "$GPRMC,";
 
 
 
 void GPS_Read(){
+	char i,Recieved_char,GPSCOUNTER=0,flag = 1 ;
 
-char Recieved_char;
-char flag = 1 ;
-
-do{
-	flag = 1 ;
-	for(char i=0 ;i<7;i++)
-	{ 
-		if(MCAL_UART_ReadChar(UART0) != GPS_LogName[i])      // get_charFn from uart
-		{
-			flag = 0;
-			break ;
+	do{
+		flag = 1 ;
+		for(i=0 ;i<7;i++)
+		{ 
+			Recieved_char = MCAL_UART_ReadChar(UART2);
+			if(Recieved_char != GPS_LogName[i])      // get_charFn from uart
+			{
+				flag = 0;
+				break ;
+			}
 		}
 	}
-} while (flag == 0);
- 
-                       // i make sure that i get the correct log 
-
-
-strcpy(GPS , "");           //init gps 
-
-do
-{
-	char GPSCOUNTER ; 
+	while (flag == 0);	// i make sure that i get the correct log 
 	
-	Recieved_char = MCAL_UART_ReadChar(UART0);
-	
-	GPS[GPSCOUNTER++] = Recieved_char;
-}while(Recieved_char!='*');
-
+	strcpy(GPS , "");           //init gps 
+	do
+	{	
+		Recieved_char = MCAL_UART_ReadChar(UART2);
+		GPS[GPSCOUNTER++] = Recieved_char;
+	}
+	while(Recieved_char!='*');
 }
-void GPS_FORMAT()
+
+void GPS_FORMAT(float* arr)
 {
 	char counter = 0;
 	
@@ -70,51 +60,55 @@ void GPS_FORMAT()
 	}while(token!=NULL);
 		
 	
-if(strcmp(GPS_formated[1],"A")== 0)// valid gps output 
-	
-{
-	if(strcmp(GPS_formated[3],"N")==0)
-		currentLat = atof(GPS_formated[2]);// if was N then it is + 
-	else 
-	currentLat = -atof(GPS_formated[2]);
-	if(strcmp(GPS_formated[5],"E")==0)
+	if(strcmp(GPS_formated[1],"A")== 0)			// valid gps output 
+	{
+		if(strcmp(GPS_formated[3],"N")==0)
+			currentLat = atof(GPS_formated[2]);// if was N then it is + 
+		else 
+			currentLat = -atof(GPS_formated[2]);
 		
-		currentlong = atof(GPS_formated[4]);
-	else
-		currentlong = - atof(GPS_formated[4]);
-}
+		if(strcmp(GPS_formated[5],"E")==0)
+			
+			currentlong = atof(GPS_formated[4]);
+		else
+			currentlong = - atof(GPS_formated[4]);
+		
+		
+		 arr[0] 	= currentLat;
+		 arr[1]   = currentlong;
+	}
 }
 		
-	
-
 float ToDegree (float angle){
 	int degree = (int)angle/100 ; 
 	float minutes = angle -(float)degree*100;
-return (degree + (minutes/60) );
+	return (degree + (minutes/60) );
 }	
-	float ToRad(float angle){
-		return angle * pi/180 ;
-	}
+
+
+float ToRad(float angle){
+	return angle * pi/180;
+}
 	
 	// get angle from degree to rad 
 
 
-	float GPS_GetDistance (float currentLong,float currentLat, float destlong,float destlat){
-	   	float currentLongRad = ToRad(ToDegree(currentLong));
-			float currentLatRad = ToRad(ToDegree(currentLat));
-			float destlongRad = ToRad(ToDegree(destlong));
-			float destlatRad = ToRad(ToDegree(destlat));
-		
-		//get diff  
-		
-		float longDiff = destlongRad - currentLongRad ;
-		float latDiff  = destlatRad - currentLatRad ; 
-		// calculate distance 
-		float a = pow(sin(latDiff)/2,2)+ cos(currentLatRad)*cos(destlatRad)*pow(sin(longDiff)/2,2);// haversine formula a = sin2((qb-qa)/2)+ cos(qa)*cos(qb)*sin2((ib-ia)/2)
-		//where q is latitude and i is longitude of points a and b 
-		
-		double c = 2*atan2(sqrt(a),sqrt(1-a));
-		return EARTH_RADIUS * c ; 
-	}
+float GPS_GetDistance (float currentLong,float currentLat, float destlong,float destlat){
+   	float currentLongRad = ToRad(ToDegree(currentLong));
+	float currentLatRad = ToRad(ToDegree(currentLat));
+	float destlongRad = ToRad(ToDegree(destlong));
+	float destlatRad = ToRad(ToDegree(destlat));
+	
+	//get diff  
+	float longDiff = destlongRad - currentLongRad ;
+	float latDiff  = destlatRad - currentLatRad ; 
+	
+	// calculate distance 
+	float a = pow(sin(latDiff)/2,2)+ cos(currentLatRad)*cos(destlatRad)*pow(sin(longDiff)/2,2);// haversine formula a = sin2((qb-qa)/2)+ cos(qa)*cos(qb)*sin2((ib-ia)/2)
+	
+	//where q is latitude and i is longitude of points a and b 
+	double c = 2*atan2(sqrt(a),sqrt(1-a));
+	return EARTH_RADIUS * c ; 
+}
 	
 			
--- Drivers/HAL/GPS/gps.h
+++ Drivers/HAL/GPS/gps.h
@@ -7,8 +7,9 @@
 #define pi 3.14159265359
 #define  EARTH_RADIUS 6371000
 
+
 void GPS_Read(); // to check if the array is correct ?
-void GPS_FORMAT() ;// split the gps_output array into tokens
+void GPS_FORMAT(float* arr) ;// split the gps_output array into tokens
 float ToDegree (float angle); //distance to degree
 float ToRad(float angle);// angle in degree to rad 
 float GPS_GetDistance (float currentLong,float currentLat, float destlong,float destlat);// distance fn (The Haversine Formula)
