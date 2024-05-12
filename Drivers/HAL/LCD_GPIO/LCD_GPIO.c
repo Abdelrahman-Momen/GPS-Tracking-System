93ee37ac2672e969caec85ebb45a2b20e9db1035
Mohamed Elsheshtawy <120197885+MElsheshtawy@users.noreply.github.com>
2024-05-12 04:26:59 +0300

    Update LCD_GPIO.c

--- Drivers/HAL/LCD_GPIO/LCD_GPIO.c
+++ Drivers/HAL/LCD_GPIO/LCD_GPIO.c
@@ -1,86 +1,102 @@
 #include "LCD_GPIO.h"
+#include "string.h"
+#include "stdio.h"
+#include "SYSTICK.h"
 
-void SEND_ON_PINS(uint8_t data,uint8_t rs){
-	MCAL_GPIO_Write_Pin(PORTB,RS_PIN,rs);
+void LCD_GPIO_INIT()
+{
+		int i =0;
+		MCAL_GPIO_Init(PORTE);
+		for(i=1;i<4;i++){
+			MCAL_GPIO_Digital_INIT(PORTE,i);
+			MCAL_GPIO_Digital_PIN_MODE(PORTE,i,Output,1);
+		} 
+		MCAL_GPIO_Init(PORTB);
+		for(i=4;i<=7;i++){
+			MCAL_GPIO_Digital_INIT(PORTB,i);
+			MCAL_GPIO_Digital_PIN_MODE(PORTB,i,Output,1);
+		} 
+}
+
+void LCD_INIT()
+{	
+		LCD_GPIO_INIT();
 	
-	MCAL_GPIO_Write_Pin(PORTB,D7_PIN,(data>>3)&1);
-	MCAL_GPIO_Write_Pin(PORTB,D6_PIN,(data>>2)&1);
-	MCAL_GPIO_Write_Pin(PORTB,D5_PIN,(data>>1)&1);
-	MCAL_GPIO_Write_Pin(PORTB,D4_PIN,(data>>0)&1);
+		LCD_Send_CMD(0x33);
+		LCD_Send_CMD(0x32);
+		LCD_Send_CMD(0x28);	
+		LCD_Send_CMD(0x01);
+		LCD_Send_CMD(0x0C);                        		                  	  
+}
+void LCD_Send_CMD(uint8_t data)
+{
 	
-	MCAL_GPIO_Write_Pin(PORTB,EN_PIN,1);
-	systick_delay(2);
-	MCAL_GPIO_Write_Pin(PORTB,EN_PIN,0);
-	systick_delay(2);
-}
+	  MCAL_GPIO_Write_Pin(PORTE,RS_PIN,0);
+    MCAL_GPIO_Write_Pin(PORTE,RW_PIN,0);
 
-
-void LCD_SEND_CMD(uint8_t cmd){
-	uint8_t firsthalf, secondhalf;
-	firsthalf = ((cmd>>4)&0xf);
-	secondhalf = ((cmd)&0xf);
-	SEND_ON_PINS(firsthalf,0);
-	SEND_ON_PINS(secondhalf,0);
+    MCAL_GPIO_Write_Pin(PORTE,EN_PIN,1);
+    systick_delay(1);                
+		GPIO_PORTB_DATA = (GPIO_PORTB_DATA&0x0F)|(data&0xF0);
+    systick_delay(1);                  
+    MCAL_GPIO_Write_Pin(PORTE,EN_PIN,0);
+    systick_delay(1);                 
+    MCAL_GPIO_Write_Pin(PORTE,EN_PIN,1);
+    systick_delay(1);                
+		GPIO_PORTB_DATA = (GPIO_PORTB_DATA&0x0F)|((data&0xF)<<4);
+    systick_delay(1);                   
+    MCAL_GPIO_Write_Pin(PORTE,EN_PIN,0);
+    systick_delay(1);        
 }
-void LCD_SEND_DATA(uint8_t data){
-	uint8_t firsthalf, secondhalf;
-	firsthalf = ((data>>4)&0xf);
-	secondhalf = ((data)&0xf);
-	SEND_ON_PINS(firsthalf,1);
-	SEND_ON_PINS(secondhalf,1);
-}
+void LCD_Send_Data(uint8_t data)
+{
+		MCAL_GPIO_Write_Pin(PORTE,RS_PIN,1);
+    MCAL_GPIO_Write_Pin(PORTE,RW_PIN,0);
 
-void LCD_INIT(){
-	// init gpio
-	int i =0;
-	MCAL_GPIO_Init(PORTB);
-	for(i=0;i<=7;i++){
-		if(i==3)continue;
-		MCAL_GPIO_Digital_INIT(PORTB,i);
-		MCAL_GPIO_Digital_PIN_MODE(PORTB,i,Output,1);
-	}
-	// init LCD in 4 bit mode
-	systick_delay(50);
-	LCD_SEND_CMD(0x30);
-	systick_delay(5);
-	LCD_SEND_CMD(0x30);
-	systick_delay(1);
-	LCD_SEND_CMD(0x30);
-	systick_delay(20);
-	LCD_SEND_CMD(0x20);
-	systick_delay(20);
+    MCAL_GPIO_Write_Pin(PORTE,EN_PIN,1);
+    systick_delay(1);
+		GPIO_PORTB_DATA = (GPIO_PORTB_DATA&0x0F)|(data&0xF0);
+    systick_delay(1);                  
+    MCAL_GPIO_Write_Pin(PORTE,EN_PIN,0);
+    systick_delay(1);                 
+    MCAL_GPIO_Write_Pin(PORTE,EN_PIN,1);
+    systick_delay(1);                
+		GPIO_PORTB_DATA = (GPIO_PORTB_DATA&0x0F)|((data&0xF)<<4);
+    systick_delay(1);                   
+    MCAL_GPIO_Write_Pin(PORTE,EN_PIN,0);
+    systick_delay(1);
+}
 	
-	//dislay initialisation
-	LCD_SEND_CMD (0x28);
-	systick_delay(1);
-	LCD_SEND_CMD (0x08);
-	systick_delay(1);
-	LCD_SEND_CMD(0x01); 
-	systick_delay(2); 
-	LCD_SEND_CMD(0x06); 
-	systick_delay (1);
-	LCD_SEND_CMD(0x0C); 
-	systick_delay (1);
-}
-void LCD_CLEAR(){
-	LCD_SEND_CMD(0x1);
-	systick_delay(2);
-}
-
-
-void LCD_SEND_STRING(uint8_t* string){
-	int i=0;
-	while(string[i] != '\0'){
-		LCD_SEND_DATA(string[i]);
-		i++;
-		systick_delay (1);
+void LCD_SEND_STRING(uint8_t * str)
+{
+	uint8_t counter = 0;
+	while(str[counter] != '\0'){
+		LCD_Send_Data(str[counter]);
+		counter++;
 	}
+		
 }
-void LCD_PUT_CURSOR(uint8_t row,uint8_t column){
-	if(row){
-		LCD_SEND_CMD(column|0x80);
-	}
-	else{
-		LCD_SEND_CMD(column|0xC0);
-	}
+void HAL_LCD_Send_Num(double num)
+{
+	  char str[5] = {0};
+    sprintf(str, "%.2f", num);
+    LCD_SEND_STRING(str);
+}
+void LCD_ClR()
+{
+	 LCD_Send_CMD(0x01);
+}
+void LCD_SetCursor(uint8_t x, uint8_t y)
+{
+		uint8_t cursor;
+		
+    switch(x)
+    {
+        case 0:
+                cursor=y;
+                break;
+        case 1:
+                cursor = y+0x40;
+                break;
+    }
+    LCD_Send_CMD(cursor | 0x80);
 }
